b35b5a78c02fc9975ebc4af73a27a0c74ba77232
helenailic <hilic@stu.naperville203.org>
2021-11-13 15:55:37 -0600

    try2

--- firstViz.js
+++ firstViz.js
@@ -1,1 +1,6 @@
-console.log("Hello! :)")
+let width = 970
+let height = 700
+var svg =
+d3.select("#viz").append("svg").attr("width",width).attr("height",height).style("width",width).style("height",height)
+svg.append("circle").attr("cx",50).attr("cy",50).attr("r",40).attr("fill","lavander")
+svg.append("rectangle").attr("x",120).attr("y",10).attr("width",200).attr("height",80).attr("fil","lightblue")
